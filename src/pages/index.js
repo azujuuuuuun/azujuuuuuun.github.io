022b8c934e2da4f172f2dbaf27db308e9e85f717
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-03 23:45:45 +0900

    ページ一覧を追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,38 @@
 import React from "react"
+import { Link, graphql } from 'gatsby'
 
 import Header from '../components/Header'
 
-export default () => (
+export default ({ data }) => (
   <div>
     <Header />
     <div>Hello world!</div>
+    <ul>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <li key={node.id}>
+          <Link to={node.fields.slug}>
+            {node.frontmatter.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
