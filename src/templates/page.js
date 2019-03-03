dbf04c612f58b1164217ec8f2ab7be11e00a2313
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-04 00:14:16 +0900

    Helmetを追加

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Helmet } from 'react-helmet'
 
 import Header from '../components/Header'
 
@@ -7,6 +8,10 @@ export default ({ data }) => {
   const { markdownRemark } = data
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{`${markdownRemark.frontmatter.title} | azujuuuuuun.github.io`}</title>
+      </Helmet>
       <Header />
       <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
     </div>
@@ -17,6 +22,9 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      frontmatter {
+        title
+      }
     }
   }
 `
