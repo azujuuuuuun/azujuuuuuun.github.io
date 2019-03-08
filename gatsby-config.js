6987a2396da153a29ff569eaa19299d87efd18ce
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-08 22:42:37 +0900

    Googleアナリティクスの設定を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,18 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-128961201-4',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      }
+    }
   ]
 }
