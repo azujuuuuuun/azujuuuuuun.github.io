abd8104722a43256929441b97b9a62e0a01799ef
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 00:36:20 +0900

    フッターを追加

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
     <Header />
     {children}
+    <Footer />
   </div>
 )
