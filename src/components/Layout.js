802bde2f81c714ff4dac627f3b9994307d66afd7
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-10 01:04:23 +0900

    isHomeプロパティを受け渡すように変更

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-export default ({ children }) => (
+export default ({ isHome, children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    <Header />
+    <Header isHome={isHome} />
     {children}
     <Footer />
   </div>
