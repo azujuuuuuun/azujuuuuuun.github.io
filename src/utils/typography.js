3729ae1b3323c77f21f1469795517d732fafb9f8
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-10 00:25:01 +0900

    Typography.jsのテーマをGitHubに変更

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import fairyGateTheme from 'typography-theme-fairy-gates'
+import githubTheme from 'typography-theme-github'
 
-const typography = new Typography(fairyGateTheme)
+const typography = new Typography(githubTheme)
 
 export const { scale, rhythm, options } = typography
 export default typography
