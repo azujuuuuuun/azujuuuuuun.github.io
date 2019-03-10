213cdef5cfdadceca9f2f7a8bcb849c41c6e506c
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 19:19:15 +0900

    gatsby-remark-imagesとgatsby-plugin-sharpの設定を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,20 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
