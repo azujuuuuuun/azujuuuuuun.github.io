d4cc0bd87333bfaf58f959baa7b176c1e59836d3
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 22:37:56 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5f69e00620d9f841858d.js"
+    "url": "webpack-runtime-fb782d608cfbad83eb81.js"
   },
   {
     "url": "styles.2e3965805b9561b25985.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d8b5899223e707f29c8b5c9c320169f5"
+    "revision": "88c72ff653ac16187c1ed4e219bcf477"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
