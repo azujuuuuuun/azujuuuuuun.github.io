ed4f22e3c7139f43607be66982d6da290e26b608
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 22:20:29 +0900

    GraphQLのqueryを削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
 import Layout from '../components/Layout'
 
-export default ({ data }) => (
+export default () => (
   <Layout isHome>
     <Helmet>
       <meta charSet="utf-8" />
@@ -21,31 +21,6 @@ export default ({ data }) => (
       <li>
         <Link to="/favorites">好きなもの</Link>
       </li>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <li key={node.id}>
-          <Link to={node.fields.slug}>
-            {node.frontmatter.title}
-          </Link>
-        </li>
-      ))}
     </ul>
   </Layout>
 )
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: frontmatter___page, order: ASC }) {
-      edges {
-        node {
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-          }
-        }
-      }
-    }
-  }
-`
