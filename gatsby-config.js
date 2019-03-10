df8f1bbecbdf031bea64f0186f3f3c688837f023
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 16:38:55 +0900

    ダブルクォーテーションをシングルクォーテーションに修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
         theme_color: '#1f0d80',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline',
