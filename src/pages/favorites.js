9e5b84ac2088280de9ca10a3604f67df452f068c
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-03-10 22:10:33 +0900

    好きなものページコンポーネントを追加

---
+++ src/pages/favorites.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>好きなもの | azujuuuuuun.github.io</title>
+    </Helmet>
+    <h1>好きなもの</h1>
+    <p>漫画と映画とアニメとプログラミングが好きです。</p>
+    <h2>漫画</h2>
+    <ul>
+      <li>違国日記</li>
+      <li>Artiste</li>
+      <li>彼方のアストラ</li>
+      <li>金色のガッシュベル</li>
+      <li>ちはやふる</li>
+      <li>ハイキュー!!</li>
+      <li>バクマン。</li>
+      <li>ヒナまつり</li>
+      <li>BLUE GIANT</li>
+      <li>僕のヒーローアカデミア</li>
+      <li>ランウェイで笑って</li>
+    </ul>
+    <h2>映画</h2>
+    <ul>
+      <li>イエスマン　“YES”は人生のパスワード</li>
+      <li>きっと、うまくいく</li>
+      <li>グレイテスト・ショーマン</li>
+      <li>最強のふたり</li>
+      <li>世界から猫が消えたなら</li>
+      <li>セッション</li>
+    </ul>
+    <h2>アニメ</h2>
+    <ul>
+      <li>四月は君の嘘</li>
+      <li>SHIROBAKO</li>
+      <li>風が強く吹いている</li>
+    </ul>
+    <h2>その他</h2>
+    <ul>
+      <li>Red Bull</li>
+      <li>LAWSONのカフェオレグランデ</li>
+    </ul>
+  </Layout>
+)
