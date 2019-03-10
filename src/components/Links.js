18438d38156f0210a52fe4509d5be439d9ab0ef4
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-10 23:10:45 +0900

    Linksコンポーネントを追加

---
+++ src/components/Links.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default props => {
+  const { listStyle, itemStyle, linkStyle } = props
+  return (
+    <ul style={listStyle}>
+      <li style={itemStyle}>
+        <Link to="/about" style={linkStyle}>自己紹介</Link>
+      </li>
+      <li style={itemStyle}>
+        <Link to="/skills" style={linkStyle}>スキル</Link>
+      </li>
+      <li style={itemStyle}>
+        <Link to="/favorites" style={linkStyle}>好きなもの</Link>
+      </li>
+      <li style={itemStyle}>
+        <Link to="/links" style={linkStyle}>リンク</Link>
+      </li>
+    </ul>
+  )
+}
