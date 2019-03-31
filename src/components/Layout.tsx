cc10789a4c308b68c102980510bb3fc076b8543d
azujuuuuuun <azujuuuuuun@gmail.com>
2019-03-31 22:08:32 +0900

    propsの型を追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-export default ({ isHome, children }) => (
+interface Props {
+  isHome?: boolean
+  children: React.ReactNode
+}
+
+export default ({ isHome, children }: Props) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
     <Header isHome={isHome} />
     {children}
