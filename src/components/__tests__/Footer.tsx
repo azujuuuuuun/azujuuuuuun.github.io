55b0d867d2f1737f52bd576caf8b2ea4e516557f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-04-01 22:19:29 +0900

    :white_check_mark: Footerコンポーネントのテストを追加

---
+++ src/components/__tests__/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'jest'
+import renderer from 'react-test-renderer'
+
+import Footer from '../Footer'
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Footer />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
