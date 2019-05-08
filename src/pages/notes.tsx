f2c3b4a3e9d64b5ef3e8d4ac39edfab5536c3a51
azujuuuuuun <azujuuuuuun@gmail.com>
2019-05-09 00:44:09 +0900

    :sparkles: メモページを追加

---
+++ src/pages/notes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Head from '../components/Head';
+
+export default (): JSX.Element => (
+  <Layout>
+    <Head title="メモ | azujuuuuuun.github.io" />
+    <h1>メモ</h1>
+    <p>開発中</p>
+  </Layout>
+);
