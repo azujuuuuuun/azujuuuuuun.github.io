8d1f60188e2efecdaadd63f09f557e55dbb5ed88
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2019-05-10 00:37:10 +0900

    :sparkles: メモの一覧を追加

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Link, graphql } from "gatsby";
 
 import Layout from '../components/Layout';
 import Head from '../components/Head';
 
-export default (): JSX.Element => (
+export default ({ data }): JSX.Element => (
   <Layout>
     <Head title="メモ | azujuuuuuun.github.io" />
     <h1>メモ</h1>
-    <p>開発中</p>
+    {data.allMarkdownRemark.edges.map(({ node }): JSX.Element => (
+      <div key={node.id}>
+        <Link to={node.fields.slug}>
+          <h3>
+            {node.frontmatter.title}{' '}
+            <span>
+              - {node.frontmatter.published}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </Link>
+      </div>
+    ))}
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___id], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            published
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
