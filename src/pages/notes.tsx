07ead42e2e5a849cb73b690536b049313d61251f
azujuuuuuun <azujuuuuuun@gmail.com>
2019-05-11 23:10:24 +0900

    :art: レイアウトコンポーネントのディレクトリを移動

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout';
+import Layout from '../layouts/DefaultLayout';
 import Head from '../components/Head';
 
 export default ({ data }): JSX.Element => (
--- src/templates/note.tsx
+++ src/templates/note.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 
-import Layout from "../components/layout";
+import Layout from "../layouts/DefaultLayout";
 import Head from '../components/Head';
 
 export default ({ data }): JSX.Element => {
