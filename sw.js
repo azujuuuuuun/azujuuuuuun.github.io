1bcac510a19fabba67b8b0d2a72552e9d19e969a
azujuuuuuun <azujuuuuuun@gmail.com>
2019-05-12 23:56:51 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4805b4efd6f28e3eee65.js"
+    "url": "webpack-runtime-d2d2f1244aca49e71a61.js"
   },
   {
     "url": "styles.6b9b38cf90ff19047730.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6d686cb189acbcc83709f7f4e018fdec"
+    "revision": "02e8ae0b1525b6ee0cc9f35944e31ef4"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
