fc51388c9b092eed3726fa8480918ba4d897d13c
azujuuuuuun <azujuuuuuun@users.noreply.github.com>
2022-08-23 17:16:03 +0000

    deploy: eee7edf1c5ab425aa05da83abef6f58672f88c26

---
+++ _next/static/LYHi35xy8Hk4D0MWqtAoV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef8a27c3f8e33e08.css","static/chunks/pages/index-b8dd1a02c5e75867.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
