3ffbbb1597f8ccdc37e4a6c4cdd8510c0738076f
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-08-24 00:26:40 +0900

    Reduce favorite contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,105 +41,11 @@ const HomePage: React.FC = () => {
           </section>
           <section>
             <h2>Favorite</h2>
-            <p>漫画と映画とアニメとプログラミングが好きです。</p>
-            <section>
-              <h3>漫画</h3>
-              <ul>
-                <li>違国日記</li>
-                <li>Artiste</li>
-                <li>彼方のアストラ</li>
-                <li>金色のガッシュベル</li>
-                <li>ちはやふる</li>
-                <li>ハイキュー!!</li>
-                <li>バクマン。</li>
-                <li>ヒナまつり</li>
-                <li>BLUE GIANT</li>
-                <li>僕のヒーローアカデミア</li>
-                <li>ランウェイで笑って</li>
-              </ul>
-            </section>
-            <section>
-              <h3>映画</h3>
-              <ul>
-                <li>イエスマン “YES”は人生のパスワード</li>
-                <li>きっと、うまくいく</li>
-                <li>
-                  <a
-                    href="http://www.foxmovies-jp.com/greatest-showman/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    グレイテスト・ショーマン
-                  </a>
-                </li>
-                <li>最強のふたり</li>
-                <li>世界から猫が消えたなら</li>
-                <li>
-                  <a
-                    href="http://session.gaga.ne.jp/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    セッション
-                  </a>
-                </li>
-              </ul>
-            </section>
-            <section>
-              <h3>アニメ</h3>
-              <ul>
-                <li>
-                  <a
-                    href="http://kazetsuyo-anime.com/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    風が強く吹いている
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.kimiuso.jp/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    四月は君の嘘
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://shirobako-anime.com/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    SHIROBAKO
-                  </a>
-                </li>
-              </ul>
-            </section>
-            <section>
-              <h3>その他</h3>
-              <ul>
-                <li>
-                  <a
-                    href="https://energydrink-jp.redbull.com/red-bull-energy-drink"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    Red Bull
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.lawson.co.jp/recommend/original/detail/1303294_1996.html"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    LAWSONのカフェオレグランデ
-                  </a>
-                </li>
-              </ul>
-            </section>
+            <ul>
+              <li>Manga</li>
+              <li>Anime</li>
+              <li>Programming</li>
+            </ul>
           </section>
           <section>
             <h2>Link</h2>
