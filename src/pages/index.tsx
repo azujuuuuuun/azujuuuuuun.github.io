dae3e743776081834693ee2cf38484bbd179e7c5
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-08-24 00:21:55 +0900

    Add viewport meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ const HomePage: React.FC = () => {
       <Head>
         <title>azujuuuuuun.github.io</title>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
       </Head>
       <div
