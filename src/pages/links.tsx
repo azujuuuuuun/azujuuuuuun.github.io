fc19dc231854189e968ddb4993a01f7979111276
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-08-23 13:52:49 +0900

    Delete links

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -27,42 +27,6 @@ const LinksPage: React.VFC = () => (
           Twitter
         </a>
       </li>
-      <li>
-        <a
-          href="https://qiita.com/azujuuuuuun"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Qiita
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://booklog.jp/users/azujuuuuuun"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ブクログ
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://eiga.com/mypage/753513/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          映画.com
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://animetick.net/users/azujuuuuuun"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Animetick
-        </a>
-      </li>
     </ul>
   </Layout>
 );
