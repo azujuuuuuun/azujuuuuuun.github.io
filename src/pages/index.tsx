a364ae055a0e591a4e30a3cc861f95064bed3aa8
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-08-25 00:44:05 +0900

    Add Custom Document component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,6 @@ const HomePage: React.FC = () => {
     <>
       <Head>
         <title>azujuuuuuun.github.io</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="favicon.ico" />
       </Head>
       <div className={styles.container}>
         <Header />
