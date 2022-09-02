ee7cad3010aa3bd2204942e883e39dd4a5eb57e9
azujuuuuuun <azujuuuuuun@users.noreply.github.com>
2022-09-02 16:07:55 +0000

    deploy: 8f44c67f558467f8f692080267df5d89bc91a9b5

--- _next/static/2686YAVuWiKxWrR6o0-rs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/87a34d18c4c6daa5.css","static/chunks/pages/index-a06c22d148f3d14f.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/QE9grUGHpF0uZHcT1FNj4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a8694b99ec31b569.css","static/chunks/pages/index-6a313864374a0175.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
