ddd11e46f17966613251a61827ed72a5ff4a4e72
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-11 00:01:50 +0900

    Add fetchFeed error test

--- src/api/fetch-feed.test.ts
+++ src/api/fetch-feed.test.ts
@@ -1,10 +1,16 @@
 import { Readable } from "stream";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { logger } from "@/lib/logger";
 import { client, fetchFeed } from "./fetch-feed";
 
 jest.mock("@aws-sdk/client-s3");
+jest.mock("@/lib/logger");
 
 describe("fetchFeed", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("returns feed", async () => {
     // setup
     const body = Readable.from('{"title":"title"}');
@@ -20,5 +26,25 @@ describe("fetchFeed", () => {
     expect(GetObjectCommand).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledTimes(1);
     expect(result).toEqual(expected);
+  });
+
+  it("logs error on error", async () => {
+    expect.assertions(4);
+
+    // setup
+    const error = new Error("error message");
+    const mockSend = jest.fn().mockRejectedValue(error);
+    const spy = jest.spyOn(client, "send").mockImplementation(mockSend);
+
+    try {
+      // execute
+      await fetchFeed();
+    } catch (e) {
+      // assert
+      expect(GetObjectCommand).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(logger.error).toHaveBeenCalledTimes(1);
+      expect(e).toEqual(error);
+    }
   });
 });
