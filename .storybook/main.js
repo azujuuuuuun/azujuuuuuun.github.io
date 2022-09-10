884aedba27d1fa510757aaca30028794470a0239
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-10 22:41:10 +0900

    Add About story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -15,6 +17,12 @@ module.exports = {
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
     });
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
 
     return config;
   },
---
+++ src/modules/About/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { About } from ".";
+
+export default {
+  title: "Modules/About",
+  component: About,
+} as ComponentMeta<typeof About>;
+
+const Template: ComponentStory<typeof About> = (args) => <About {...args} />;
+
+export const Default = Template.bind({});
