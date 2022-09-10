362bae8aca080e5a379a7c2a0ee20f69d70107e2
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-10 13:31:49 +0900

    Add Footer test

---
+++ src/modules/Footer/index.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Footer } from ".";
+
+describe("Footer", () => {
+  it("renders update date", () => {
+    const date = new Date("2022-09-10").toJSON();
+
+    render(<Footer updateDate={date} />);
+
+    expect(screen.queryByText(/2022\/09\/10/)).not.toBeNull();
+  });
+});
