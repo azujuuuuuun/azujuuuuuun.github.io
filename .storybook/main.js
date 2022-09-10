25ebeb4b14b642264355050d72af1bfdc825f760
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2022-09-10 22:33:24 +0900

    Configure CSS Modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,4 +9,12 @@ module.exports = {
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    });
+
+    return config;
+  },
 };
