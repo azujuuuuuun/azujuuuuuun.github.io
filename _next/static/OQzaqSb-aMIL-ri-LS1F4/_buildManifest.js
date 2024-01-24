a4cd01578fd107f7ab71f90aea7983c6ce5e454c
azujuuuuuun <azujuuuuuun@users.noreply.github.com>
2024-01-24 15:50:30 +0000

    deploy: 5b33c9dcfac224d14d9034182a58bcf6eda188d8

--- _next/static/OQzaqSb-aMIL-ri-LS1F4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/08bf7ad16f796b4f.css","static/chunks/pages/index-e4ac8ad532e7c786.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
