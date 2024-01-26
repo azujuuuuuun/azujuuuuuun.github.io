69ab6ad75c552902473213bef2127e72fe660353
azujuuuuuun <azujuuuuuun@users.noreply.github.com>
2024-01-26 14:18:47 +0000

    deploy: c697778101ced7b86a77875908aeab0a3a3c8222

--- _next/static/4jla5k6qH7xaEpW3OKOK8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b261388a795dbad3.css","static/chunks/pages/index-521bb3643d34b373.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_error-452c6a35cebcb029.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{137:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(3464)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=137)}),_N_E=n.O()}]);
