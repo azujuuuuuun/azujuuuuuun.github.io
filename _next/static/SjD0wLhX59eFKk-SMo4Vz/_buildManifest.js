4629da184fb6928be06b067698e2f62010e40ade
azujuuuuuun <azujuuuuuun@users.noreply.github.com>
2024-09-11 12:20:48 +0000

    deploy: 5ce1bc907175be53ad44fa22fb576e2a60e7bbdc

---
+++ _next/static/SjD0wLhX59eFKk-SMo4Vz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b261388a795dbad3.css","static/chunks/pages/index-1ffab6d2936920f5.js"],"/_error":["static/chunks/pages/_error-433968e1e674841b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ixqOPgtrGsAZrAKcy-Q3B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b261388a795dbad3.css","static/chunks/pages/index-c16dbc1f5a4a9a07.js"],"/_error":["static/chunks/pages/_error-452c6a35cebcb029.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
